37abd86072dbc3d59e673eeaa13d15cf704c9a7d
CodeKhoVL <21a120100049@gmail.com>
2023-03-25 08:05:38 +0700

    fixed footer

--- includes/footer.php
+++ includes/footer.php
@@ -40,25 +40,16 @@
   <div class="row align-items-center justify-content-lg-between">
     <div class="col-lg-6">
       <div class="copyright text-center  text-lg-left  text-muted">
-        &copy; 2020 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a> & <a href="https://themesberg.com?ref=creative-tim" class="font-weight-bold ml-1" target="_blank">Themesberg</a>
+        &copy; 2023 <a href="https://hou.edu.vn" class="font-weight-bold ml-1" target="_blank">HOU</a>
       </div>
     </div>
     <div class="col-lg-6">
       <ul class="nav nav-footer justify-content-center justify-content-lg-end">
         <li class="nav-item">
-          <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+          <a href="https://www.facebook.com/HOUNews" class="nav-link" target="_blank">Facebook</a>
         </li>
         <li class="nav-item">
-          <a href="https://themesberg.com" class="nav-link" target="_blank">Themesberg</a>
-        </li>
-        <li class="nav-item">
-          <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
-        </li>
-        <li class="nav-item">
-          <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
-        </li>
-        <li class="nav-item">
-          <a href="https://www.creative-tim.com/license" class="nav-link" target="_blank">License</a>
+          <a href="https://hou.edu.vn" class="nav-link" target="_blank">Discord</a>
         </li>
       </ul>
     </div>
